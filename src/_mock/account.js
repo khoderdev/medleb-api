29d610bc4fe09a73ea8eecfbbb7a3090a9bb7b46
Khoder <132526568+khoderdev@users.noreply.github.com>
2024-03-13 14:01:24 +0200

    deploy 3

--- src/_mock/account.js
+++
@@ -1,7 +0,0 @@
-// ----------------------------------------------------------------------
-
-export const account = {
-  displayName: 'Pharmacy Service',
-  email: 'demo@demo.com',
-  photoURL: '/assets/images/avatars/avatar_25.jpg',
-};
--- src/sections/user/view/index.js
+++
@@ -1,2 +0,0 @@
-export { default as UserView } from './user-view';
-export { default as NewUserView } from './user-form';
